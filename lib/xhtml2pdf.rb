daa504addc03202997264d95ef477d51603fef05
John Sheets <john.sheets@space150.com>
2009-09-03 15:08:03 -0500

    Invoke Flying Saucer natively in JRuby, when available.

--- lib/xhtml2pdf.rb
+++ lib/xhtml2pdf.rb
@@ -13,17 +13,31 @@ module ActsAsFlyingSaucer
       File.open(options[:input_file], 'w') do |file|
         file << options[:html]
       end
-     
-      java_dir = File.join(File.expand_path(File.dirname(__FILE__)), "java")
-     
-      class_path = ".:#{java_dir}/bin"
 
-      Dir.glob("#{java_dir}/jar/*.jar") do |jar|
-        class_path << "#{options[:classpath_separator]}#{jar}"
+      if defined?(JRUBY_VERSION)
+        input = options[:input_file]
+        output = options[:output_file]
+        url = java.io.File.new(input).toURI.toURL.toString
+
+        os = java.io.FileOutputStream.new(output)
+
+        renderer = org.xhtmlrenderer.pdf.ITextRenderer.new
+        renderer.setDocument(url)
+        renderer.layout
+        renderer.createPDF(os)
+        os.close
+      else
+        java_dir = File.join(File.expand_path(File.dirname(__FILE__)), "java")
+
+        class_path = ".:#{java_dir}/bin"
+
+        Dir.glob("#{java_dir}/jar/*.jar") do |jar|
+          class_path << "#{options[:classpath_separator]}#{jar}"
+        end
+
+        command = "#{options[:java_bin]} -cp #{class_path} Xhtml2Pdf #{options[:input_file]} #{options[:output_file]}"
+        system(command)
       end
-
-      command = "#{options[:java_bin]} -cp #{class_path} Xhtml2Pdf #{options[:input_file]} #{options[:output_file]}"
-      system(command)
     end
   end
 end
